f8cdab4380c08cce5bf0b7bb2aea083fcd2a4a8a
James Rodgers <myfavoritenoisemaker@gmail.com>
2019-01-25 19:40:36 -0700

    Update fileSync.js

--- fileSync.js
+++ fileSync.js
@@ -6,7 +6,7 @@ var fileLocations = [
 	"\\\\path\\to\\file.mdb",
 	"\\\\path\\to\\file.mdb",
 	"\\\\path\\to\\file.mdb",
-]
+];
 var lastFile="C:\\Users\\path\\to\\file.mdb";//local copy of last version of file to compare to see if the master file has been changed
 var bkUpLoc="C:\\Users\\path\\to\\backup folder\\";//backups of previous files in case something goes wrong
 
@@ -14,19 +14,19 @@ let lastDate = fs.statSync(lastFile).mtimeMs;
 let currentDate = fs.statSync(masterFile).mtimeMs;
 
 getDateStamp = function(){
-	fullDate = new Date();
+    fullDate = new Date();
     d = fullDate.getDate();
     m = fullDate.getMonth()+1;
     y = fullDate.getFullYear();
-	dateStamp = [m,d,y];
-	dateStamp = dateStamp.join("-");
+    dateStamp = [m,d,y];
+    dateStamp = dateStamp.join("-");
     return dateStamp
 }
 
 syncFiles = function(){
-	for (i = 0; i < fileLocations.length; i++){
-	fs.copyFileSync(lastFile, fileLocations[i]);
-	}
+    for (i = 0; i < fileLocations.length; i++){
+        fs.copyFileSync(lastFile, fileLocations[i]);
+    }
     console.log("all files copied");
 };
 
@@ -40,10 +40,10 @@ writeLog = function(txt){
 };
 
 if (lastDate < currentDate){
-	console.log("Newer File Available");
-	stamp = getDateStamp();
+    console.log("Newer File Available");
+    stamp = getDateStamp();
     bkUpFile = bkUpLoc.concat(stamp,".mdb");
-	console.log(bkUpFile);
+    console.log(bkUpFile);
 	try {
 		fs.copyFileSync(lastFile, bkUpFile);
 		fs.copyFileSync(masterFile, lastFile);
